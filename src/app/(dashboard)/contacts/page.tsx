1a116408da0b3e85978b0bf5a72fd4b17d106000
NighwanTech <nighwantech@gmail.com>
2026-07-27 10:59:44 +0530

    drop down 1k

--- src/app/(dashboard)/contacts/page.tsx
+++ src/app/(dashboard)/contacts/page.tsx
@@ -836,6 +836,8 @@ export default function ContactsPage() {
                 <option value={25}>25</option>
                 <option value={50}>50</option>
                 <option value={100}>100</option>
+                <option value={500}>500</option>
+                <option value={1000}>1000</option>
               </select>
             </div>
           </div>
